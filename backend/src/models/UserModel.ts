24a09c811f33a6bb44b2b378ec86e35c176e7d44
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-19 14:43:58 +0530

    feat: add userSchema and pre save hook for password bypass

---
+++ backend/src/models/UserModel.ts
@@ -0,0 +1,50 @@
+import mongoose, { mongo } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    lowerCase: true,
+    trim: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: [true, `Please enter your email`],
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, `Please enter your password`],
+    trim: true,
+  },
+}, {timestamps:true});
+
+//pre save hook for saving password
+userSchema.pre("save", async function (next) {
+    //calls the function only when the password is modified or created
+    if(!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password, salt);
+    next();
+})
+
+//comparing password
+userSchema.methods.matchPassword = async function ( enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+export const uesr = mongoose.model("User", userSchema);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cors from 'cors'
 import express from "express";
 import {connectDB} from "./config/db.ts";
 import { errorHandler } from "./middlewares/errorHandler.ts";
@@ -8,7 +9,10 @@ dotenv.config();
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+server.use(express.json({ limit: "16kb" }));
+server.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+server.use(cors());
 server.use(errorHandler);
 
 server.listen(PORT, () => {
