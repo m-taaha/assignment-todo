6fa53be6de75fa9f0341abcf8ee866ec28dee40d
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-11-19 13:10:27 +0530

    feat: add errorHandler middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
 import {connectDB} from "./config/db.ts";
-
+import { errorHandler } from "./middlewares/errorHandler.ts";
 
 dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+
+server.use(errorHandler);
 
 server.listen(PORT, () => {
     console.log(`Server is running on PORT: http://localhost:${PORT}`,);
